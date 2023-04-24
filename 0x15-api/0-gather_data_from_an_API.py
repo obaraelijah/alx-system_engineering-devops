95ff652d1938b56ce6387d2115f0fc1202ef107e
elijah obara <elijahobara357@gmail.com>
2023-04-24 15:08:12 +0300

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,7 +6,7 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    user_id =argv[1]
+    user_id = argv[1]
     user_url = "https://jsonplaceholder.typicode.com/users/" + user_id
     user_dict = requests.get(user_url).json()
     user_name = user_dict.get("name")
