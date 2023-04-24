52b1f1bcb6ba49d053132acb5237f39c9d73895c
elijah obara <elijahobara357@gmail.com>
2023-04-24 21:37:12 +0300

    Export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -9,20 +9,20 @@ import sys
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
-    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    user_url = "https://jsonplaceholder.typicode.com/users/" + user_id
     user_dict = requests.get(user_url).json()
     user_name = user_dict.get("username")
     user_todo = requests.get("{}/todos".format(user_url))
     user_todo = user_todo.json()
-    file_name = f"{user_id}.json"
+    file_name = user_id + ".json"
     my_dict = {}
 
     my_dict[user_id] = []
 
-    for item in user_todo:
+    for elem in user_todo:
         inner_dict = {}
-        inner_dict["task"] = item.get("title")
-        inner_dict["completed"] = item.get("completed")
+        inner_dict["task"] = elem.get("title")
+        inner_dict["completed"] = elem.get("completed")
         inner_dict["username"] = user_name
         my_dict.get(user_id).append(inner_dict)
 
