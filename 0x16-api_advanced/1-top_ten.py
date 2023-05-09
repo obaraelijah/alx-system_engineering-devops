b80d0cfb16eff3d6afc6eb1e0dba6eb5d2d8b950
elijah obara <elijahobara357@gmail.com>
2023-05-09 22:40:57 +0300

    unction that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""This module contains a function that queries the Reddit API
+and prints the titles of the first 10 hot posts for a given subreddit
+"""
+import requests
+
+def top_ten(subreddit):
+    """Prints the titles of the first 10 hot posts listed
+    for a given subreddit
+    """
+    url = 'https://reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    limit = {'limit': 10}
+
+    response = requests.get(url, headers=headers, params=limit)
+
+    if not response:
+        print(None)
+    else:
+        posts = response.json().get('data').get('children')
+
+        for post in posts:
+            title = post.get('data').get('title')
+            print(title)
