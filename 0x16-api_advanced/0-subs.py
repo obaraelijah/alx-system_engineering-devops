04c05273d79b492d5e012b5e43eceada32ec44f0
elijah obara <elijahobara357@gmail.com>
2023-05-09 22:28:11 +0300

    function that queries the Reddit API and returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""This module contains a function that queries the Reddit API
+and returns the number of subscribers for a given subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers for an existing
+    subreddit
+    Return:
+        number of subscribers (int)
+    """
+    url = 'https://reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    response = requests.get(url, headers=header)
+
+    if not response:
+        return 0
+    else:
+        data = response.json().get('data')
+        return data.get('subscribers')
