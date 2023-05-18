a7022c39ddbb9752cc9eb3dece79706c689c8c02
elijah obara <elijahobara357@gmail.com>
2023-05-18 10:10:11 +0300

    Holberton user

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,9 @@
 # Change the OS configuration so that it is possible to login with the holberton user 
 
-exec { 'change-os-configuration-for-holberton-user':
+exec {`'change-os-configuration-for-holberton-user':
   command => "bash -c \"sed -iE 's/^holberton hard nofile \
 5/holberton hard nofile 88888/' /etc/security/limits.conf; \
 sed -iE 's/^holberton soft nofile \
 4/holberton soft nofile 88888/' /etc/security/limits.conf\"",
-  path    => '/usr/bin:/usr/sbin:/bin'
+  path => '/usr/bin:/usr/sbin:/bin'
 }
